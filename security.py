0660a6d1e654823215227992a2b191a605d6fc23
JaceRushing02201 <jar02201@ucmo.edu>
2026-04-17 12:51:59 -0500

    Comments and starting regex

--- security.py
+++ security.py
@@ -1,4 +1,6 @@
 """
+When making a user will salt and hash the created password for storage
+When logging in takes the input password and compares to hashed password
     • Dedicated to SHA-256 hashing.
     • Note: Passwords must start with a special character from !@#$%^&* and be
             6–12 characters long.
--- validator.py
+++ validator.py
@@ -1,4 +1,12 @@
 """
+Runs only when registering a User
 Contains Regex functions for names (capitalized, no digits),
-phones (xxx-xxx-xxxx), and specific email extensions (.yahoo, .gmail, .ucmo).
+phones (xxx-xxx-xxxx)
+specific email extensions (.yahoo, .gmail, .ucmo).
+And password format
 """
+
+name_regex = r""
+email_regex = r"(yahoo)(ucmo)(gmail)"
+phone_regex = r"(\d{3}-\d{3}-\d{4})"
+pass_regex = r"^[!@#$%^&*].*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,11}$"
