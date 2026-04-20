bdbbb55faed5ef4d8b50a5f38b93dafbbc1f7f09
JaceRushing02201 <jar02201@ucmo.edu>
2026-04-20 12:43:26 -0500

    Validator now compares and runs regex on names, numbers, emails, and passwords

--- validator.py
+++ validator.py
@@ -2,11 +2,27 @@
 Runs only when registering a User
 Contains Regex functions for names (capitalized, no digits),
 phones (xxx-xxx-xxxx)
-specific email extensions (.yahoo, .gmail, .ucmo).
+specific email extensions (yahoo, gmail, ucmo).
 And password format
 """
+import re
 
-name_regex = r""
-email_regex = r"(yahoo)(ucmo)(gmail)"
-phone_regex = r"(\d{3}-\d{3}-\d{4})"
-pass_regex = r"^[!@#$%^&*].*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,11}$"
+def valid_name(name):
+    name_regex = r"(^[A-Z])([a-z]{2,})"
+    if re.match(name_regex, name):
+        return True
+
+def valid_email(email):
+    email_regex = r"(yahoo)(ucmo)(gmail)"
+    if re.match(email_regex, email):
+        return True
+
+def valid_phone(phone):
+    phone_regex = r"(\d{3}-\d{3}-\d{4})"
+    if re.match(phone_regex, phone):
+        return True
+
+def valid_password(password):
+    pass_regex = r"^[!@#$%^&*].*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,11}$"
+    if re.match(pass_regex, password):
+        return True
