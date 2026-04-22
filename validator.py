55aa8c7d96d1421b824a7c85d3f5ff971b45a6e8
JaceRushing02201 <jar02201@ucmo.edu>
2026-04-22 08:38:36 -0500

    Validator can now return false if the regex fails.

--- validator.py
+++ validator.py
@@ -11,18 +11,22 @@ def valid_name(name):
     name_regex = r"(^[A-Z])([a-z]{2,})"
     if re.match(name_regex, name):
         return True
+    return False
 
 def valid_email(email):
     email_regex = r"(yahoo)(ucmo)(gmail)"
     if re.match(email_regex, email):
         return True
+    return False
 
 def valid_phone(phone):
     phone_regex = r"(\d{3}-\d{3}-\d{4})"
     if re.match(phone_regex, phone):
         return True
+    return False
 
 def valid_password(password):
     pass_regex = r"^[!@#$%^&*].*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,11}$"
     if re.match(pass_regex, password):
         return True
+    return False
